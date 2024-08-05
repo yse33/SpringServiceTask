553f1d478f97bef83ce92a88d370062b9255d91c
Yasen Angelov <yse33@abv.bg>
2024-08-05 21:23:39 +0300

    Removed User Permission Granting from Registration.

--- src/main/java/com/example/exercise/DTO/UserRegisterDTO.java
+++ src/main/java/com/example/exercise/DTO/UserRegisterDTO.java
@@ -2,13 +2,10 @@ package com.example.exercise.DTO;
 
 import lombok.Data;
 
-import java.util.Set;
-
 @Data
 public class UserRegisterDTO {
     private String username;
     private String password;
     private String firstName;
     private String surname;
-    private Set<String> permissions;
 }
--- src/main/java/com/example/exercise/mapper/UserMapper.java
+++ src/main/java/com/example/exercise/mapper/UserMapper.java
@@ -4,15 +4,10 @@ import com.example.exercise.DTO.UserDTO;
 import com.example.exercise.DTO.UserRegisterDTO;
 import com.example.exercise.DTO.UserResponseDTO;
 import com.example.exercise.model.User;
-import com.example.exercise.security.Permission;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Mapper(uses = UserMapper.class)
 public interface UserMapper {
@@ -20,14 +15,6 @@ public interface UserMapper {
 
     UserDTO toUserDTO(User user);
     List<UserDTO> toUserDTOs(List<User> users);
-    @Mapping(target = "permissions", source = "permissions", qualifiedByName = "stringSetToEnumSet")
     User fromUserRegisterDTO(UserRegisterDTO userRegisterDTO);
     UserResponseDTO toUserResponseDTO(User user);
-
-    @Named("stringSetToEnumSet")
-    default Set<Permission> stringSetToEnumSet(Set<String> permissions) {
-        return permissions.stream()
-                .map(Permission::valueOf)
-                .collect(Collectors.toSet());
-    }
 }
